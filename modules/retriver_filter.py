59a3fb1849c06b69d4338b99fbc61d4f4fed2a5e
Vivek Singh <vivek.mitmpl2023@learner.manipal.edu>
2025-10-03 13:23:45 +0530

    Improved Retriver

--- modules/retriver_filter.py
+++ modules/retriver_filter.py
@@ -13,7 +13,17 @@ class ScoreFilteredRetriever(BaseRetriever):
     class Config:
         arbitrary_types_allowed = True  # allows vectorstore to be any object
 
-    def _get_relevant_documents(self, query: str, **kwargs) -> List[Document]:
+    def _get_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_name: Optional[str] = None,
+        callbacks: Optional[List[Any]] = None,
+        tags: Optional[List[str]] = None,
+        metadata: Optional[dict] = None,
+        **kwargs,
+    ) -> List[Document]:
+    
         docs_and_scores = self.vectorstore.similarity_search_with_score(query, k=self.fetch_k or self.k)
 
         # Filter by score
@@ -31,5 +41,21 @@ class ScoreFilteredRetriever(BaseRetriever):
 
         return [d for d, _ in docs_and_scores[: self.k]]
 
-    async def _aget_relevant_documents(self, query: str, **kwargs) -> List[Document]:
-        return self._get_relevant_documents(query, **kwargs)
+    async def _aget_relevant_documents(        
+        self,
+        query: str,
+        *,
+        run_name: Optional[str] = None,
+        callbacks: Optional[List[Any]] = None,
+        tags: Optional[List[str]] = None,
+        metadata: Optional[dict] = None,
+        **kwargs,
+    ) -> List[Document]:
+        return self._get_relevant_documents(
+                query, 
+                run_name=run_name, 
+                callbacks=callbacks, 
+                tags=tags, 
+                metadata=metadata, 
+                **kwargs
+        )
